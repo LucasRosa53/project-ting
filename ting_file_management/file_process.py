9bbecee7a6a1f9ee44b8112052780813e9f5cf78
LucasRosa53 <lucasrosa223@hotmail.com>
2023-08-02 15:02:15 -0300

    resolvendo req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+queueModel = Queue()  # instância
+
+
+def process(path_file, instance: Queue):
+    file = txt_importer(path_file)
+
+    for element in instance.itens:
+        if path_file in element["nome_do_arquivo"]:
+            return
+
+    dictionary = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    instance.enqueue(dictionary)
 
 
 def remove(instance):
@@ -8,3 +26,8 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+process("statics/nome_pedro.txt", queueModel)
+process("statics/arquivo_teste.txt", queueModel)
+print(queueModel.itens)
