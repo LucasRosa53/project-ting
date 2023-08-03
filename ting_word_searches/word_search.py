39f457abe2302536f95bb7eefcc077ad5c385aff
LucasRosa53 <lucasrosa223@hotmail.com>
2023-08-03 18:55:49 -0300

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,29 @@
-def exists_word(word, instance):
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+def exists_word(word, instance: Queue):
     """Aqui irá sua implementação"""
+    result = []
+    list = instance.itens
+    for element in list:
+        ocorrencias = []
+        for i, linha in enumerate(element["linhas_do_arquivo"]):
+            if word.lower() in linha.lower():
+                ocorrencias.append({
+                    "linha": i +1
+                })
+        if ocorrencias:
+            result.append({
+                "palavra": word,
+                "arquivo": element["nome_do_arquivo"],
+                "ocorrencias": ocorrencias
+            })
+        return result
+
+
+
+
+
 
 
 def search_by_word(word, instance):
